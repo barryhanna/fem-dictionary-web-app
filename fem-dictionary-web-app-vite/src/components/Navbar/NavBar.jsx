242039a0c691ef07e76aa9983eb1ab4e698557d6
Barry Hanna <barryhanna@users.noreply.github.com>
2023-04-06 17:08:27 +0100

    Add spacer variables

--- fem-dictionary-web-app-vite/src/components/Navbar/NavBar.jsx
+++ fem-dictionary-web-app-vite/src/components/Navbar/NavBar.jsx
@@ -3,17 +3,17 @@ import ToggleButton from "../ToggleButton/ToggleButton";
 
 const NavBar = () => {
     return (
-        <nav>
+        <nav className="navbar">
             <img src='/assets/images/logo.svg' alt='' />
 
             <div className="options">
-
-            <select name="typeface" id="typeface">
-                <option value="sans">Sans Serif</option>
-                <option value="serif">Serif</option>
-                <option value="mono">Monospace</option>
-            </select>
-            <ToggleButton />
+                <select name="typeface" id="typeface">
+                    <option value="sans">Sans Serif</option>
+                    <option value="serif">Serif</option>
+                    <option value="mono">Monospace</option>
+                </select>
+                <ToggleButton />
+                <img src="/assets/images/icon-moon.svg" alt="" />
             </div>
         </nav>
     )
