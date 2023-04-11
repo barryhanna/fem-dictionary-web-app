b176310cbb9edbaf883094399cd4835cc8dbf8f1
Barry Hanna <barryahanna@gmail.com>
2023-04-11 20:26:54 +0100

    Add ability to change typeface

--- fem-dictionary-web-app-vite/src/components/Selector/Selector.jsx
+++ fem-dictionary-web-app-vite/src/components/Selector/Selector.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
 const Selector = () => {
+  const [selectedTypeface, setSelectedTypeface] =
+    React.useState('sans');
+
+  function updateTypefaceVar(newTypeface) {
+    document.body.style.setProperty(
+      '--ff-main-ff',
+      `var(--ff-${newTypeface})`
+    );
+  }
+
   return (
     <select
       className="typeface-selector"
       name="typeface"
       id="typeface"
+      value={selectedTypeface}
+      onChange={(e) => {
+        const newTypeface = e.target.value;
+        setSelectedTypeface(newTypeface);
+        updateTypefaceVar(newTypeface);
+      }}
     >
       <option value="sans">Sans Serif</option>
       <option value="serif">Serif</option>
