53b80896777695b1427cae587905118c088c0fa5
Barry Hanna <barryahanna@gmail.com>
2023-04-11 20:04:08 +0100

    Add loading spinner

--- fem-dictionary-web-app-vite/src/App.jsx
+++ fem-dictionary-web-app-vite/src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import DictionaryEntry from './components/DictionaryEntry/DictionaryEntry';
 import NavBar from './components/Navbar/NavBar';
 import SearchInput from './components/SearchInput/SearchInput';
-import { data } from './data/example-word';
+import { ClockLoader } from 'react-spinners';
 
 function App() {
   const DICTIONARY_URL =
     'https://api.dictionaryapi.dev/api/v2/entries/en/';
   const [word, setWord] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
+    setLoading(true);
     async function getWordData() {
       const res = await fetch(`${DICTIONARY_URL}${word}`);
       const json = await res.json();
@@ -20,13 +22,23 @@ function App() {
       setWord(json[0]);
     }
     getWordData();
+    setTimeout(() => setLoading(false), 500);
   }, [word]);
 
   return (
     <div className="App">
       <NavBar />
       <SearchInput word={word} setWord={setWord} />
-      <DictionaryEntry wordData={word} />
+      {loading && (
+        <div className="dictionary-entry__spinner-container">
+          <ClockLoader
+            size={200}
+            aria-label="Loading Spinner"
+            color={'var(--clr-accent)'}
+          />
+        </div>
+      )}
+      {!loading && <DictionaryEntry wordData={word} />}
     </div>
   );
 }
