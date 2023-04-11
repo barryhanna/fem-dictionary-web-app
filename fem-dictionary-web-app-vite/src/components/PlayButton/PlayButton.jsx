abcde7e7dcff80db954f4297b5e6b0e51efc969f
Barry Hanna <barryahanna@gmail.com>
2023-04-11 21:53:59 +0100

    PlayButton hover state

--- fem-dictionary-web-app-vite/src/components/PlayButton/PlayButton.jsx
+++ fem-dictionary-web-app-vite/src/components/PlayButton/PlayButton.jsx
@@ -8,7 +8,17 @@ const PlayButton = ({ audioPlayer }) => {
         audioPlayer.current.play();
       }}
     >
-      <img src="assets/images/icon-play.svg" alt="" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="75"
+        height="75"
+        viewBox="0 0 75 75"
+      >
+        <g>
+          <circle cx="37.5" cy="37.5" r="37.5" opacity=".25" />
+          <path d="M29 27v21l21-10.5z" />
+        </g>
+      </svg>
       <div className="visually-hidden">Play</div>
     </button>
   );
